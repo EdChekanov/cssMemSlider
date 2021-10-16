63658674e94afddb27568f40300e687bf0165d7c
EdChekanov <kide.da@gmail.com>
2021-10-16 14:43:53 +0300

    feat: add js (Fri, Oct 16, 2021 14.43 PM)

---
+++ cssMemSlider/script.js
@@ -0,0 +1,15 @@
+const image = document.querySelector('.slider_main');
+const controls = document.querySelectorAll('.footer_controls ul li label');
+const text = document.querySelector('.footer_text p');
+const textData = ['I listening you..?!', "It's for me?", 'Give me my beer?', 'Pretty cool!', 'Relax! Take it easy.']
+
+function changeSlide(evt) {
+  image.style.backgroundImage = `url(images/${evt.target.htmlFor}.jpg)`;
+  text.style.opacity = '0';
+  setTimeout(() => {
+    text.textContent = textData[evt.target.htmlFor];
+    text.style.opacity = '1';
+  }, 500);
+}
+
+controls.forEach(el => el.addEventListener('click', changeSlide));
